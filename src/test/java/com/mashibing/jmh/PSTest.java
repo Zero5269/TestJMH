254507c1b3c26695898182c56fedfa24edad407c
Zero5269 <945246041@qq.com>
2021-10-27 13:43:46 +0800

    update

--- src/test/java/com/mashibing/jmh/PSTest.java
+++ src/test/java/com/mashibing/jmh/PSTest.java
@@ -7,10 +7,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PSTest {
     @Benchmark
     @Warmup(iterations = 1, time = 3)
-    @Fork(5)
+    @Fork(2)
     @BenchmarkMode(Mode.Throughput)
     @Measurement(iterations = 1, time = 3)
     public void testForEach() {
+        PS.foreach();
+    }
+
+    @Benchmark
+    @Warmup(iterations = 1, time = 3)
+    @Fork(2)
+    @BenchmarkMode(Mode.AverageTime)
+    @Measurement(iterations = 1, time = 3)
+    public void testForEach1() {
         PS.foreach();
     }
 }
