8b9fc78204a7d68dcff3b88303d3c0ca960af24a
Zero5269 <945246041@qq.com>
2022-01-20 11:26:31 +0800

    update imh

--- src/test/java/com/mashibing/jmh/PSTest2.java
+++ src/test/java/com/mashibing/jmh/PSTest2.java
@@ -5,17 +5,14 @@ import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-public class PSTest {
-
+public class PSTest2 {
     @Benchmark
     @Warmup(iterations = 3, time = 1,timeUnit = TimeUnit.SECONDS)
     @Fork(3)
     @Timeout(1)
-    @Threads(4)
-    @BenchmarkMode(Mode.AverageTime)
-    @Measurement(iterations = 2, time = 1)
+    @Threads(3)
+    @BenchmarkMode(Mode.Throughput)
+    @Measurement(iterations = 1,time = 2)
     @OutputTimeUnit(TimeUnit.MILLISECONDS)
     public void testForEach() {
         PS.foreach();
